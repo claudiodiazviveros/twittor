2e356e7789814e1cb3d3d2eb0af5fa1e5df46720
Claudio Diaz Viveros <Claudio Diaz Viveros@DESKTOP-O1GGH8J>
2020-07-16 23:51:30 -0400

    Update

--- sw.js
+++ sw.js
@@ -84,5 +84,5 @@ self.addEventListener('sync', event => {
 // Start listening function in event 'push'. Receive notification.
 self.addEventListener('push', event => {
     console.log("Receive notification, event 'push'.");
-    addPost(event);
+    addPost("Receive notification, event 'push'.");
 })
